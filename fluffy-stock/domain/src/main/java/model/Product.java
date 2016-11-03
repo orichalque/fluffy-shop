efd840f9ee8b3ed3b0afa17a54279b2cc60cccad
benjimor <benji_moreau@hotmail.fr>
2016-11-03 13:27:22 +0100

    B - Domaine crée + test sur le modele

---
+++ fluffy-stock/domain/src/main/java/model/Product.java
@@ -0,0 +1,167 @@
+package model;
+
+import model.exceptions.NotEnoughProductsException;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.UUID;
+
+/**
+ * Product model class definition
+ * Define the products sold or stored in the stock
+ */
+public class Product {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private ProductType productType;
+    private double price;
+    private int quantity;
+
+    /**
+     * Return the id
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    /**
+     * Set the field id
+     *
+     * @param id as java.util.UUID
+     */
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    /**
+     * Increase the amount of products in the stock by numberToAdd
+     * @param numberToAdd the amount of item to add
+     */
+    public void increaseQuantityBy(int numberToAdd) {
+        quantity += numberToAdd;
+    }
+
+    /**
+     * Remove the amount of products in the stock by numberToRemove
+     * @param numberToRemove the amount of item to remove
+     */
+    public void decreaseQuantityBy(int numberToRemove) throws NotEnoughProductsException {
+        if (numberToRemove > quantity) {
+            throw new NotEnoughProductsException();
+        }
+        quantity -= numberToRemove;
+    }
+
+    /**
+     * Return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the field name
+     *
+     * @param name as java.lang.String
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Set the field description
+     *
+     * @param description as java.lang.String
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Return the productType
+     */
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    /**
+     * Set the field productType
+     *
+     * @param productType as model.ProductType
+     */
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+
+    /**
+     * Return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * Set the field price
+     *
+     * @param price as double
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Set the field quantity
+     *
+     * @param quantity as int
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Product rhs = (Product) obj;
+
+        //The item are the same if their id are the same
+        return new EqualsBuilder()
+                .append(this.id, rhs.id)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(id)
+                .append(name)
+                .append(description)
+                .append(productType)
+                .append(price)
+                .append(quantity)
+                .toHashCode();
+    }
+}
---
+++ fluffy-stock/domain/src/main/java/model/ProductType.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Define the different possible types for the products stored in the stock
+ * The usage of an enumeration instead of a specific java object allows to add types
+ * after the creation of the model, and increase the maintainability
+ */
+public enum ProductType {
+
+}
