deb1321f629306dc5f3e70c03c3339d8c91facb7
orichalque <thiblf@gmail.com>
2016-11-09 15:51:24 +0100

    T - Dépendances ajoutées dans les pom.xml

--- fluffy-stock/domain/src/main/java/model/ProductType.java
+++ fluffy-stock/domain/src/main/java/model/ProductType.java
@@ -1,10 +1,40 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Define the different possible types for the products stored in the stock
  * The usage of an enumeration instead of a specific java object allows to add types
  * after the creation of the model, and increase the maintainability
  */
-public enum ProductType {
+public enum ProductType {;
+
+    private final String value;
+    private final static Map<String, ProductType> CONSTANTS = new HashMap<String, ProductType>();
+
+    static {
+        for (ProductType c: values()) {
+            CONSTANTS.put(c.value, c);
+        }
+    }
+
+    private ProductType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    public static ProductType fromValue(String value) {
+        ProductType constant = CONSTANTS.get(value);
+        if (constant == null) {
+            throw new IllegalArgumentException(value);
+        } else {
+            return constant;
+        }
+    }
 
 }
