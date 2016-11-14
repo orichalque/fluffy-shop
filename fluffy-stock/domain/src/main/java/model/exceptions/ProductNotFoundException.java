6adea4c7d82871fb4e2bb291dc725afa56cf5143
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-11-14 14:14:33 +0100

    T - refactoring de l'arborescence

--- fluffy-stock/domain/src/main/java/model/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package model.exceptions;
-
-/**
- * Exception raised when trying to update or delete an item not existing in the database
- */
-public class ProductNotFoundException extends Exception {
-}
--- fluffy-stock/infrastructure/src/test/java/com/alma/groupe8/config/CommonTestVariables.java
+++
@@ -1,8 +0,0 @@
-package com.alma.groupe8.config;
-
-/**
- * Created by Thibault on 09/11/2016.
- */
-public class CommonTestVariables {
-    public static final String COLLECTION_NAME = "test";
-}
