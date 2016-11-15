99ab73c6bbdc83fc719b42d3d2ea302fbf9e1ec1
orichalque <thiblf@gmail.com>
2016-11-15 14:31:16 +0100

    T - Correction d'un override inutile

--- infrastructure/src/main/java/com/alma/groupe8/repository/MongoRepository.java
+++ infrastructure/src/main/java/com/alma/groupe8/repository/MongoRepository.java
@@ -50,12 +50,6 @@ public class MongoRepository implements ProductsRepository {
     }
 
     @Override
-    public Collection<String> findProductsByType(String type) {
-        //TODO
-        return null;
-    }
-
-    @Override
     public void store(String product) throws AlreadyExistingProductException {
         Document document = Document.parse(product);
         String currentProduct = find(document.getString("id").toString());
