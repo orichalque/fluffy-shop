c8972a66a1eab7c12b7c8b9b1d0339afb0e25a9c
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-11-24 13:44:07 +0100

    T - ajout de config pour le CORS

--- application/src/main/java/com/alma/group8/configuration/ApplicationConfiguration.java
+++ application/src/main/java/com/alma/group8/configuration/ApplicationConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -49,8 +50,13 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
     }
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
