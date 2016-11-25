4209d14745208fdec7615c1f83117dc6d1c59e6a
dennisman <dennisman@laposte.net>
2016-11-25 16:12:21 +0100

    D - ajout d'une interface factory pour la serialization

---
+++ api/src/main/java/com/alma/group8/interfaces/ProductFactory.java
@@ -0,0 +1,22 @@
+package com.alma.group8.interfaces;
+
+/**
+ * Convert Business object to String and String to Business object
+ * Created by Dennis on 25/11/16.
+ */
+public interface ProductFactory<Metier>{
+
+    /**
+     * Convert the string param to a business Object
+     * @param dto the serialize object in Json stringify format
+     * @return business object corresponding to the string param
+     */
+     Metier deserialize(String dto);
+
+    /**
+     * Serialize a business object
+     * @param object to serialize
+     * @return the object in Json stringify
+     */
+     String serialize(Metier object);
+}
